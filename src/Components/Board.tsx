600961f53ae9a7f108f56253ca0a3f328f5e2e79
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-04-15 02:55:20 +0900

    To Do List (2)
    Design add

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import DraggableCard from "./DraggableCard";
 import { useForm } from "react-hook-form";
 import { IToDo } from "../atom";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { toDoState } from "../atom";
 
 const Wrapper = styled.div`
@@ -23,28 +23,24 @@ const Title = styled.div`
   font-weight: 600;
   padding-bottom: 10px;
   font-size: 2em;
-  background-color: white;
-  width: 100%;
+  //background-color: white;
+  width: 90%;
   margin: 0 auto;
+  padding-top: 10px;
   border-bottom: 2px solid black;
-  margin-bottom: 20px;
+  //margin-bottom: 20px;
 `;
 
-interface IAreaProps {
+/* interface IAreaProps {
   isDraggingOver: boolean;
   draggingFromThisWith: boolean;
 }
+ */
 
-const Area = styled.div<IAreaProps>`
-  background-color: ${(props) =>
-    props.isDraggingOver
-      ? "pink"
-      : props.draggingFromThisWith
-      ? "red"
-      : "	#6091E8"};
-  transition: background-color 0.3s ease-in-out;
+const Area = styled.div`
   flex-grow: 1;
-  padding: 20px;
+  padding: 5%;
+  //background-color: red;
 `;
 
 interface IBoardProps {
@@ -58,57 +54,88 @@ interface IForm {
 
 const Form = styled.form`
   width: 100%;
-  input {
-    width: 100%;
+  //background-color: red;
+  display: inline-block;
+  text-align: center;
+  height: 8%;
+`;
+
+const Input = styled.input`
+  width: 90%;
+  height: 80%;
+  margin: 0 auto;
+  background-color: rgba(0, 0, 0, 0);
+  border: 1px solid black;
+  color: whitesmoke;
+  outline: none;
+  ::placeholder {
+    color: black;
   }
 `;
 
+const Add = styled.div`
+  font-size: 2em;
+  font-weight: 600;
+  width: 90%;
+  margin: 0 auto;
+  padding-bottom: 10px;
+`;
+
 function Board({ toDos, boardId }: IBoardProps) {
-  const setToDos = useSetRecoilState(toDoState);
+  const [ToDos, setToDos] = useRecoilState(toDoState);
+  //const setToDos = useSetRecoilState(toDoState);
   const { register, setValue, handleSubmit } = useForm<IForm>();
   const onValid = ({ toDo }: IForm) => {
-    const newToDo = {
-      id: Date.now(),
-      text: toDo,
-    };
-    setValue("toDo", "");
-    setToDos((allBoard) => {
-      return { ...allBoard, [boardId]: [...allBoard[boardId], newToDo] };
-    });
+    //console.log(ToDos["Todo List"].length);
+    if (ToDos["Todo List"].length < 9) {
+      const newToDo = {
+        id: Date.now(),
+        text: toDo,
+      };
+      setValue("toDo", "");
+      setToDos((allBoard) => {
+        return { ...allBoard, [boardId]: [...allBoard[boardId], newToDo] };
+      });
+    }
   };
 
   return (
     <Wrapper>
-      {boardId === "Todo List" ? (
-        <Form onSubmit={handleSubmit(onValid)}>
-          <input
-            {...register("toDo", { required: true })}
-            type="text"
-            placeholder={`Add task on ${boardId}`}
-          />
-        </Form>
-      ) : null}
       <Droppable droppableId={boardId}>
         {(magic, snapshot) => (
-          <Area
-            isDraggingOver={snapshot.isDraggingOver}
-            draggingFromThisWith={Boolean(snapshot.draggingFromThisWith)}
-            ref={magic.innerRef}
-            {...magic.droppableProps}
-          >
+          <>
             <Title>{boardId}</Title>
-            {toDos.map((toDo, index) => (
-              <DraggableCard
-                key={toDo.id}
-                index={index}
-                toDoId={toDo.id}
-                toDoText={toDo.text}
-              />
-            ))}
-            {magic.placeholder}
-          </Area>
+            <Area ref={magic.innerRef} {...magic.droppableProps}>
+              {toDos.map((toDo, index) => (
+                <DraggableCard
+                  key={toDo.id}
+                  index={index}
+                  toDoId={toDo.id}
+                  toDoText={toDo.text}
+                />
+              ))}
+              {magic.placeholder}
+            </Area>
+          </>
         )}
       </Droppable>
+      {boardId === "Todo List" ? (
+        <>
+          <Add>Add</Add>
+          <Form onSubmit={handleSubmit(onValid)}>
+            <Input
+              {...register("toDo", { required: true })}
+              type="text"
+              placeholder={`Add task on ${boardId}`}
+            />
+          </Form>
+        </>
+      ) : (
+        <>
+          <Add style={{ color: "#6091e8" }}>blank</Add>
+          <Form></Form>
+        </>
+      )}
     </Wrapper>
   );
 }
--- src/Components/DraggableCard.tsx
+++ src/Components/DraggableCard.tsx
@@ -3,16 +3,50 @@ import styled from "styled-components";
 import React from "react";
 
 const Card = styled.div`
-  background-color: white;
-  border-radius: 5px;
-  padding: 10px 10px;
-  margin-bottom: 5px;
+  //background-color: white;
+  background-color: #6091e8;
+  border: 1px solid black;
+  //padding: 10px 10px;
+  margin-bottom: 7px;
+  height: 9%;
+`;
+
+const Left = styled.div`
+  //background-color: red;
+  padding: 0px 0px;
+  margin: 0px;
+  width: 85%;
+  height: 100%;
+  float: left;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+  font-size: 0.8em;
+`;
+
+const Right = styled.div`
+  //background-color: blue;
+  padding: 0px;
+  margin: 0px;
+  width: 15%;
+  height: 100%;
+  float: left;
+  border-left: 1px solid black;
 `;
 
 interface IDraggableCardProps {
   toDoId: number;
   toDoText: string;
   index: number;
+}
+
+{
+  /* <Area
+              isDraggingOver={snapshot.isDraggingOver}
+              draggingFromThisWith={Boolean(snapshot.draggingFromThisWith)}
+              ref={magic.innerRef}
+              {...magic.droppableProps}
+            ></Area> */
 }
 
 function DraggableCard({ toDoId, toDoText, index }: IDraggableCardProps) {
@@ -24,7 +58,8 @@ function DraggableCard({ toDoId, toDoText, index }: IDraggableCardProps) {
           {...magic.draggableProps}
           {...magic.dragHandleProps}
         >
-          {toDoText}
+          <Left>{toDoText}</Left>
+          <Right></Right>
         </Card>
       )}
     </Draggable>
